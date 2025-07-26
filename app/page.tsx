d898a2ad799f5d766f329d402a2bd3a2583b4a74
Eliziany de Oliveira Santos <eliziany.santos239@gmail.com>
2025-07-26 09:39:35 -0400

    Aula

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default async function Page() {
   return (
     <>
 
+<<<<<<< HEAD
       <div className="bg-sky-500 h-screen w-55 absolute">
         <h1>Menu</h1>
         <h2>Inicio</h2>
@@ -38,12 +39,40 @@ export default async function Page() {
                 <h1>Descrição:{item.description}</h1>
                
               </div>
+=======
+      <div className="bg-gray-400  h-screen  w-60 absolute  text-white pl-3 pt-10 ">
+        <h1 className="font-bold text-2xl">Menu</h1>
+        <h2>Inicio</h2>
+        <h2>Produtos</h2>
+        <h2>Categorias</h2>
+        <h2>Contato</h2>
+
+      </div>
+
+
+      <div className="ml-60 flex flex-wrap p-5 bg-gray-200">
+        {dados.map((item, index) =>
+          <div key={item.id}>
+            <div >
+              <div className=" flex-col  flex flex-wrap gap-4 p-4 m-4 bg-white  items-center w-100 h-190 object-cover">
+                  <div className="w-50 h-60 object-cover ">
+                    <img src={item.image} />
+                  </div>
+                <p className="pt-20 font-bold text-left"> Titulo: {item.title}</p>
+                <p className="text-emerald-500 font-bold"> Valor: {item.price}</p>
+                <h1 className="text-xs">Descrição:{item.description}</h1>
+                
+               <button className="  flex justify-center border-2 w-50 bg-blue-600 text-white">Ver Mais</button>
+              
+               </div>
+>>>>>>> 777df15 (Aula)
 
             </div>
           </div>
         )}
       </div>
 
+<<<<<<< HEAD
       {/* <div id="container" className="flex">
 
         <div id="esquerda" className="bg-blue-500 m-20" >
@@ -77,6 +106,9 @@ export default async function Page() {
         </div>
       </div> */}
 
+=======
+      
+>>>>>>> 777df15 (Aula)
     </>
   );
 
